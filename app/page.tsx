c04b1c827da6f7b34d309ae926935cbea44086a2
LucasSantamaria76 <luc_1976@hotmail.com>
2024-03-23 22:47:55 -0300

    update cardProduct

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,25 @@ import { IProduct } from './interfaces/products';
 import CreateSupabaseServerClient from './supabase/server';
 
 const getProducts = async () => {
-	try {
-		const supabase = await CreateSupabaseServerClient();
+  try {
+    const supabase = await CreateSupabaseServerClient();
 
-		const { data, error } = await supabase.from('products').select('*,products_in_stock(*)');
+    const { data, error } = await supabase.from('products').select('*,products_in_stock(*)');
 
-		if (error) throw new Error(error.message);
+    if (error) throw new Error(error.message);
 
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default async function Home() {
-	const products = await getProducts();
+  const products = await getProducts();
 
-	return (
-		<div className='flex flex-wrap w-11/12 mx-auto gap-2 mt-8'>
-			{products &&
-				products.map((product: IProduct) => <CardProduct key={product.product_id} {...product} />)}
-		</div>
-	);
+  return (
+    <div className='flex flex-wrap w-11/12 mx-auto gap-2 mt-8'>
+      {products && products.map((product: IProduct) => <CardProduct key={product.product_id} {...product} />)}
+    </div>
+  );
 }
